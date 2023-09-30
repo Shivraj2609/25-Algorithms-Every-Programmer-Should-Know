5c8e8bbac06b52b32128f2c96d358efa9808acd8
Shivraj2609 <78201801+Shivraj2609@users.noreply.github.com>
2023-10-01 00:30:00 +0530

    Update InbuiltRevSort.java
    updated inbuiltrevsort

--- 2 . Sorting/InbuiltRevSort.java
+++ 2 . Sorting/InbuiltRevSort.java
@@ -24,3 +24,17 @@ public class InbuiltRevSort {
         System.out.println();
     }
 }
+
+*/The provided Java program demonstrates how to use the `Arrays.sort` method along with `Collections.reverseOrder()` to sort an array of integers in descending order. Here's a short description of its working:
+
+1. An array of integers `arr` is created, containing the values {64, 25, 12, 22, 11}.
+
+2. The program prints the original array using the `printArray` method, displaying "Original Array."
+
+3. The `Arrays.sort` method is used with `Collections.reverseOrder()` as the comparator. This combination sorts the `arr` array in descending order, meaning the largest elements will come first.
+
+4. The program then prints the sorted array using the `printArray` method, displaying "Sorted Array (Descending Order)."
+
+5. The `printArray` method is a utility function that prints the elements of an Integer array, separated by spaces.
+
+As a result, the program will output the original array followed by the sorted array, showing the elements in descending order./*
