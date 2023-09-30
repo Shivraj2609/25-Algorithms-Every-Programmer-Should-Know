9aae37176fd4863260f9d20b812f40c8283d6c56
Shivraj2609 <78201801+Shivraj2609@users.noreply.github.com>
2023-10-01 00:15:00 +0530

    Update BubbleSort.java
    Updated BubbleSort

--- 2 . Sorting/BubbleSort.java
+++ 2 . Sorting/BubbleSort.java
@@ -44,3 +44,18 @@ public class BubbleSort {
         System.out.println();
     }
 }
+
+/*Description : 
+The provided code is a Java program that demonstrates the Bubble Sort algorithm to sort an array of integers in ascending order. Here's a short description of the code:
+
+1. The `bubbleSort` function takes an array of integers as input and sorts it using the Bubble Sort algorithm. Bubble Sort repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order. This process is repeated until no more swaps are needed, indicating that the array is sorted.
+
+2. In the `main` function:
+   - It initializes an array with unsorted integer values.
+   - It prints the original unsorted array.
+   - It calls the `bubbleSort` function to sort the array in ascending order.
+   - It prints the sorted array.
+
+3. The `printArray` function is a utility function used to print the elements of an array.
+
+The program provides a basic example of the Bubble Sort algorithm, which is not the most efficient sorting algorithm but is easy to understand and implement. It can be useful for educational purposes and for sorting small arrays.*/
